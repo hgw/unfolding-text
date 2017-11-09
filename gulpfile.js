d466b5b7f1b85aed0dd6df813a4ab2c9688590f8
hgw <register@shunyahagiwara.com>
2017-11-09 21:37:21 +0900

    gulp タスクを整理

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-let errorHandler;
 
 const pkg = require('./package.json');
 const gulp = require('gulp');
@@ -9,7 +8,7 @@ const del = require('del');
 const stripDebug = require('gulp-strip-debug');
 const uglify = require('gulp-uglify');
 const notifier = require('node-notifier');
-const gutil = require('gulp-util');
+// const gutil = require('gulp-util');
 const browserify = require('browserify');
 const babelify = require('babelify');
 const source = require('vinyl-source-stream');
@@ -19,30 +18,28 @@ const handlebars = require('gulp-compile-handlebars');
 const rename = require('gulp-rename');
 const htmlmin = require('gulp-htmlmin');
 const wait = require('gulp-wait');
+
 const compass = require('gulp-compass');
 const plumber = require('gulp-plumber');
 
 
-const path = {};
-path.root = './';
-path.src = './src';
-path.cache = './src/.cache';
-path.dist = './dist';
-path.textSrc = './text-src/index.yaml';
-
-const options = {};
-options.minify = false;
-
+const path = {
+  root: './',
+  src: './src',
+  cache: './src/.cache',
+  dist: './dist',
+  textSrc: './text-src/index.yaml',
+};
 
 /** =======================================================
- * adding ID to html elements
+ * Utility
  ======================================================= */
 let setHbsIDs = function (target_, level_) {
   for (let i = 0; i < target_.length; i += 1) {
     let current = target_[i];
     let currentLevel = level_ + "" + i;
     current["id"] = currentLevel;
-    console.log("    " + current["id"], current.text);
+    // console.log("    " + current["id"], current.text);
     if (current.children) {
       setHbsIDs(current.children, currentLevel + "-");
     }
@@ -50,106 +47,14 @@ let setHbsIDs = function (target_, level_) {
 };
 
 
-/** =======================================================
- * Define tasks
- ======================================================= */
-
-/**
- * error
- * common error handler
- */
-errorHandler = function (error) {
-  return notifier.notify({
-    message: error.message,
-    title: error.plugin,
-    sound: 'Glass'
-  });
-};
-
-
-/**
- * clean / del
- */
-
-gulp.task('clean', del.bind(null, [path.cache + '/**/*.js']));
-
-
-/*
- * console.log を削除
- */
-gulp.task('strip-debug', function () {
-  return gulp.src(path.dist + '/js/*.js').pipe(stripDebug()).pipe(gulp.dest(path.dist + '/js/'));
-});
-
-
-/*
- * jsの圧縮
- */
-gulp.task('compress', function () {
-  return gulp.src(path.dist + '/js/*.js').pipe(uglify({
-    preserveComments: 'all'
-  })).pipe(gulp.dest(path.dist + '/js/'));
-});
-
-
-/**
- * JS
- */
-gulp.task('js', () => {
-  return browserify({entries: `${path.src}/js/app.js`})
-    .transform(babelify.configure({presets: ['es2015']}))
-    .bundle()
-    .on('error', function (err) {
-      console.log(err.message);
-      console.log(err.codeFrame);
-      this.emit('end');
-    })
-    .pipe(source('app.js'))
-    .pipe(buffer())
-    .pipe(options.minify ? uglify() : gutil.noop())
-    .pipe(gulp.dest(`${path.dist}/js`));
-});
-
-/**
- *
- * YAML to JSON
- *
- */
-gulp.task('yaml-to-json', function () {
-  gulp.src(path.textSrc)
-    .pipe(yaml({
-      schema: 'DEFAULT_SAFE_SCHEMA'
-    }))
-    .pipe(gulp.dest(path.src + '/data/'))
-});
-
-gulp.task('wait-a-second', function () {
-  return gulp.src(path.textSrc)
-    .pipe(wait(500));
-});
-
-
-/**
- * handlebars をコンパイルする
- */
-gulp.task('handlebars-compile', () => {
-  console.log(path.dist+ 'に書き出します');
-  return handlebarsCompile(path.src+'/data/index.json', 'index.html', path.cache +'/html');
-});
-
-
-let handlebarsCompile = function (src, filename, destPath) {
+let doCompileHandlebars = function (src, fileName, distPath) {
   // json のデータを読み込み
   let fs = require('fs');
   let hbsModel = JSON.parse(fs.readFileSync(src, 'utf-8'));
   console.log("handlebars 変換用に json に対して ID を設定します");
   setHbsIDs(hbsModel.body, "");
 
-  // ***************************
-  // hbsModel.config.fqdn = "/";
-
   let options = {
-  //   batch: [path.src + '/hbs/partials'],
     helpers: {
       isExistBothValues: function (v1, v2, options) {
         if (v1 || v2) {
@@ -163,16 +68,38 @@ let handlebarsCompile = function (src, filename, destPath) {
 
   return gulp.src(path.src + '/handlebars/tpl.hbs')
     .pipe(handlebars(hbsModel, options))
-    // .pipe(handlebars(hbsModel))
-    .pipe(rename(filename))
-    .pipe(gulp.dest(destPath));
+    .pipe(rename(fileName))
+    .pipe(gulp.dest(distPath));
 };
 
 
+/** =======================================================
+ * Delete
+ ======================================================= */
 
-gulp.task('compass-deploy', function () {
+/**
+ * clean / del
+ */
+gulp.task('clean', del.bind(null, [
+  path.cache + '/**/*.html',
+  path.cache + '/**/*.js',
+  path.cache + '/**/*.css'
+]));
+
+
+/** =======================================================
+ * CSS / compass
+ ======================================================= */
+
+gulp.task('deploy-css', function () {
   return gulp.src(path.src + '/scss/*.scss').pipe(plumber({
-    errorHandler: errorHandler
+    errorHandler: function (error) {
+      return notifier.notify({
+        message: error.message,
+        title: error.plugin,
+        sound: 'Glass'
+      });
+    }
   })).pipe(compass({
     config_file: './config.rb',
     css: path.dist + '/css/',
@@ -184,16 +111,132 @@ gulp.task('compass-deploy', function () {
 });
 
 
+/** =======================================================
+ * Javascript
+ ======================================================= */
+
+/**
+ * Javascript: remove console.log
+ */
+gulp.task('strip-debug', function () {
+  return gulp.src(path.dist + '/js/*.js')
+    .pipe(stripDebug())
+    .pipe(gulp.dest(path.dist + '/js/'));
+});
+
+
+/*
+ * Javascript: compress
+ */
+gulp.task('compress', function () {
+  return gulp.src(path.dist + '/js/*.js')
+    .pipe(uglify({
+      preserveComments: 'all'
+    }))
+    .pipe(gulp.dest(path.dist + '/js/'));
+});
+
+
+/**
+ * Javascript: compile
+ */
+gulp.task('js-browserify', () => {
+  return browserify({
+    entries: `${path.src}/js/app.js`
+  })
+    .transform(babelify.configure({presets: ['es2015']}))
+    .bundle()
+    .on('error', function (err) {
+      console.log(err.message);
+      console.log(err.codeFrame);
+      this.emit('end');
+    })
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(uglify()) // gutil.noop()
+    .pipe(gulp.dest(`${path.dist}/js`));
+});
+
+
+/** =======================================================
+ * YAML / Handlebars
+ ======================================================= */
+
+/**
+ * YAML Document to JSON
+ */
+gulp.task('yaml-to-json', function () {
+  gulp.src(path.textSrc)
+    .pipe(yaml({
+      schema: 'DEFAULT_SAFE_SCHEMA'
+    }))
+    .pipe(gulp.dest(path.src + '/data/'))
+});
+
+/**
+ * Handlebars
+ */
+gulp.task('compile-handlebars', () => {
+  return doCompileHandlebars(path.src + '/data/index.json', 'index.html', path.cache + '/html');
+});
+
+
+/** =======================================================
+ * HTML
+ ======================================================= */
 
 /**
  * HTML Minify
  */
-gulp.task('minify', function () {
+gulp.task('minify-html', function () {
   return gulp.src(path.cache + '/html/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true,
+      preserveLineBreaks: true,
+    }))
     .pipe(gulp.dest(path.dist));
 });
 
+
+/** =======================================================
+ * Notify
+ ======================================================= */
+
+gulp.task('nf-start-watching', function () {
+  return notifier.notify({
+    title: pkg.name,
+    message: 'Start: Watching...'
+  });
+});
+
+gulp.task('nf-complete-js', function () {
+  return notifier.notify({
+    title: pkg.name,
+    message: 'Complete: Javascript!'
+  });
+});
+
+gulp.task('nf-complete-css', function () {
+  return notifier.notify({
+    title: pkg.name,
+    message: 'Complete: Compass!'
+  });
+});
+
+gulp.task('nf-complete-deploy', function () {
+  return notifier.notify({
+    title: pkg.name,
+    message: 'Complete: Deploy!'
+  });
+});
+
+gulp.task('nf-complete-yaml', function () {
+  return notifier.notify({
+    title: pkg.name,
+    message: 'Complete: Handlebars!'
+  });
+});
 
 /**
  * Watch
@@ -206,7 +249,7 @@ gulp.task('watch', function (callback) {
     function (e) {
       return runSequence(
         'compass-dev',
-        "notify-complete_compass"
+        "nf-complete-css"
       );
     });
   gulp.watch(
@@ -214,7 +257,7 @@ gulp.task('watch', function (callback) {
       `${path.src}/js/**/*.js`
     ], [
       'js',
-      "notify-complete_js"
+      "nf-complete-js"
     ]);
   gulp.watch(
     [
@@ -225,55 +268,13 @@ gulp.task('watch', function (callback) {
       return runSequence(
         'clean',
         'yaml-to-json',
-        'wait-a-second',
-        'handlebars-compile',
-        'minify',
-        'js',
-        "notify-complete_yaml"
+        'compile-handlebars',
+        'minify-html',
+        'js-browserify',
+        "nf-complete-yaml"
       );
     }
   );
-});
-
-
-/**
- *
- * notify
- *
- */
-gulp.task('notify-start_watching', function () {
-  return notifier.notify({
-    title: pkg.name,
-    message: 'Start: Watching!'
-  });
-});
-
-gulp.task('notify-complete_js', function () {
-  return notifier.notify({
-    title: pkg.name,
-    message: 'Complete: Javascript'
-  });
-});
-
-gulp.task('notify-complete_compass', function () {
-  return notifier.notify({
-    title: pkg.name,
-    message: 'Complete: Compass'
-  });
-});
-
-gulp.task('notify-complete_deploy', function () {
-  return notifier.notify({
-    title: pkg.name,
-    message: 'Complete: Deploy'
-  });
-});
-
-gulp.task('notify-complete_yaml', function () {
-  return notifier.notify({
-    title: pkg.name,
-    message: 'Complete: YAML and Handlebars!'
-  });
 });
 
 
@@ -284,12 +285,11 @@ gulp.task('default', function () {
   return runSequence(
     'clean',
     'yaml-to-json',
-    'compass-deploy',
-    'wait-a-second',
-    'handlebars-compile',
-    'minify',
-    'js',
-    "notify-start_watching",
+    'deploy-css',
+    'compile-handlebars',
+    'minify-html',
+    'js-browserify',
+    "nf-start-watching",
     "watch"
   );
 });
