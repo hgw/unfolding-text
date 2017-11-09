3f65a7a6856ec258ece6c09c7580261432b9f2c1
hgw <register@shunyahagiwara.com>
2017-11-09 22:31:39 +0900

    remove unused js classes

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,12 @@
 
 import $ from 'jquery';
 import jQuery from 'jquery';
-
 import Tlp from './tlp/tpl';
 
 window.$ = $;
 window.jQuery = jQuery;
 
 
-/**
- *
- * todo: 全て開く機能
- *
- */
 export default class App {
   constructor() {
     this._tlp = new Tlp({
